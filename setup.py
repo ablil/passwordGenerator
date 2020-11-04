e0a831cd979d5b66a4ec01f523248ae5dbeae164
ablil <ablil@protonmail.com>
2020-11-04 04:22:04 -0500

    version 0.0.1: publish to PyPi

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    readme = file.read()
+
+
+setuptools.setup(
+    name="quick-password-generator",
+    version="0.0.1",
+    author="ablil",
+    author_email="ablil@protonmail.com",
+    packages=setuptools.find_packages(),
+    entry_points={"console_scripts": ["password = src.app:main"]},
+    description="quick password generator with cache support",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/quick-password-generator",
+    classifers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
